479764be178a6947646a6b77f5d68ba7b4063c47
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-07-09 18:31:31 +0300

    feat: add header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 export default function App() {
   return (
     <>
+      <header className="header">
+        <h1 className="header__title">
+          <span className="header__connect">Connect</span>
+          <span className="header__four">Four</span>
+        </h1>
+      </header>
     </>
   );
 }
