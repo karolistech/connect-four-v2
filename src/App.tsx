38ea7639e573cc689f471f801687a5495cb00064
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-07-09 20:01:12 +0300

    feat: add board top

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+const cols = 7;
+
 export default function App() {
   return (
     <>
@@ -7,6 +9,20 @@ export default function App() {
           <span className="header__four">Four</span>
         </h1>
       </header>
+
+      <div className="board">
+        <div className="board__top">
+          {Array.from({ length: cols }, (_, colIndex) => (
+            <div key={colIndex} className="board__column">
+              <button className="board__btn">
+                <svg viewBox="0 0 16 16" width="16" height="16" className="board__arrow">
+                  <path d="M0 4 L16 4 L8 12 Z" />
+                </svg>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
