bdbeb25d37b8e196dca36cbbc099fc3c9a009602
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-07-09 18:04:58 +0300

    chore: init

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement === null) {
+  throw new Error("Root element not found");
+}
+
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
