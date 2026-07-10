e7cec35fd944220fa8006ca7443eb1357322150e
Karolis J. <264249313+karolistech@users.noreply.github.com>
2026-07-10 12:08:31 +0300

    feat: add board

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
+type Player = "red" | "yellow";
+type Cell = Player | null;
+type Board = Cell[][];
+
 const cols = 7;
+const rows = 6;
+
+function createBoard(): Board {
+  return Array.from({ length: rows }, () => Array(cols).fill(null));
+}
 
 export default function App() {
+  const [board, setBoard] = useState(createBoard);
+
   return (
     <>
       <header className="header">
@@ -21,6 +34,13 @@ export default function App() {
               </button>
             </div>
           ))}
+        </div>
+
+        <div className="board__grid">
+          {board.map((row, rowIndex) => row.map((cell, colIndex) => (
+            <div key={`${rowIndex}-${colIndex}`} className="board__cell">
+            </div>
+          )))}
         </div>
       </div>
     </>
